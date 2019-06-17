29a1ec9f612f431476ab910d2df960e93f90621e
grzegorz <gorczykowski@soldevelo.com>
2019-06-18 00:15:06 +0200

    Migrate to AndroidX

--- app/src/main/java/org/odk/collectTester/activities/AbstractActivity.java
+++ app/src/main/java/org/odk/collectTester/activities/AbstractActivity.java
@@ -18,7 +18,7 @@ package org.odk.collectTester.activities;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 
 import org.odk.collectTester.R;
